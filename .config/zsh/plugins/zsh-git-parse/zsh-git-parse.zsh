af878794a7a3dc3e102a3cf1d6448a8d96696f03
Willa Charlotte Lyle <willa@bbd.co.za>
2024-04-12 11:10:39 +0200

    feat: add 'no upstream' info to git prompt string

--- .config/zsh/plugins/zsh-git-parse/zsh-git-parse.zsh
+++ .config/zsh/plugins/zsh-git-parse/zsh-git-parse.zsh
@@ -1,17 +1,18 @@
-# MY FIRST ATTEMPT AT A PLUGIN LMAO BE GENTLE
-
 # declare ZGP vars
 declare -x ZGP_IS_GIT
 declare -x ZGP_BRANCH=$(git rev-parse --abbrev-ref HEAD 2> /dev/null)
 declare -ix ZGP_STATUS_UNTRACKED=0
 declare -ix ZGP_STATUS_STAGED=0
 declare -ix ZGP_STATUS_UNSTAGED=0
+declare -x ZGP_BRANCH_HAS_UPSTREAM
 declare -x ZGP_STATUS_BRANCH_UP_TO_DATE
 declare -ix ZGP_STATUS_BRANCH_AHEAD=0
 declare -ix ZGP_STATUS_BRANCH_BEHIND=0
+
 declare -x ZGP_STAGED_SYM='+'
 declare -x ZGP_UNSTAGED_SYM='!'
 declare -x ZGP_UNTRACKED_SYM='?'
+declare -x ZGP_BRANCH_NO_UPSTREAM_SYM='×'
 declare -x ZGP_BRANCH_UP_TO_DATE_SYM='≡'
 declare -x ZGP_BRANCH_AHEAD_SYM='↑'
 declare -x ZGP_BRANCH_BEHIND_SYM='↓'
@@ -38,12 +39,18 @@ handle_status_branch() {
   done
 }
 
-# assign ZGP_IS_GIT a value
-git rev-parse --is-inside-work-tree > /dev/null 2> /dev/null
+git rev-parse --is-inside-work-tree &> /dev/null
 if [[ $? -eq 0 ]]; then
   ZGP_IS_GIT=true
 else
   ZGP_IS_GIT=false
+fi
+
+git rev-parse --abbrev-ref $ZGP_BRANCH{upstream} &> /dev/null
+if [[ $? -eq 0 ]]; then
+  ZGP_BRANCH_HAS_UPSTREAM=true
+else
+  ZGP_BRANCH_HAS_UPSTREAM=false
 fi
 
 # parse porcelain status into STATUSES array
@@ -55,7 +62,7 @@ unset STATUS_RAW
 for STATUS in "${STATUSES[@]}";
 do
   local CODE=$(echo $STATUS | cut -c 1-2)
-  if [[ $CODE =~ ^##$ ]]; then
+  if [[ ZGP_BRANCH_HAS_UPSTREAM == true && $CODE =~ ^##$ ]]; then
     handle_status_branch $STATUS
   fi
   if [[ $CODE =~ ^[[:space:]].$ ]]; then
@@ -71,7 +78,7 @@ done
 unset STATUSES
 
 # set up-to-date var
-if [[ $ZGP_STATUS_BRANCH_AHEAD -eq 0 && $ZGP_STATUS_BRANCH_BEHIND -eq 0 ]]; then
+if [[ $ZGP_BRANCH_HAS_UPSTREAM == true && $ZGP_STATUS_BRANCH_AHEAD -eq 0 && $ZGP_STATUS_BRANCH_BEHIND -eq 0 ]]; then
   ZGP_STATUS_BRANCH_UP_TO_DATE=true
 else
   ZGP_STATUS_BRANCH_UP_TO_DATE=false
@@ -79,53 +86,48 @@ fi
 
 autoload -Uz add-zsh-hook
 
-gen_git_message () {
+gen_git_prompt () {
   [[ -f $ZDOTDIR/plugins/zsh-git-parse/zsh-git-parse.plugin.zsh ]] && source $ZDOTDIR/plugins/zsh-git-parse/zsh-git-parse.plugin.zsh > /dev/null 2> /dev/null
 
-  if [[ "$ZGP_IS_GIT" == true ]]; then
-    ZGP_PROMPT="on $ZGP_BRANCH"
-
-    if [[ $ZGP_STATUS_BRANCH_UP_TO_DATE == true ]]; then
-      ZGP_PROMPT+=" $ZGP_BRANCH_UP_TO_DATE_SYM"
-    else
-      if [[ $ZGP_STATUS_BRANCH_AHEAD -gt 0 ]]; then
-        ZGP_PROMPT+=" $ZGP_BRANCH_AHEAD_SYM$ZGP_STATUS_BRANCH_AHEAD"
-      fi
-      if [[ $ZGP_STATUS_BRANCH_BEHIND -gt 0 ]]; then
-        ZGP_PROMPT+=" $ZGP_BRANCH_BEHIND_SYM$ZGP_STATUS_BRANCH_BEHIND"
-      fi
-    fi
-
-    if [[ $ZGP_STATUS_STAGED -ne 0 || $ZGP_STATUS_UNSTAGED -ne 0 || $ZGP_STATUS_UNTRACKED -ne 0 ]]; then
-      ZGP_PROMPT+=" |"
-      if [[ $ZGP_STATUS_STAGED -ne 0 ]]; then
-        ZGP_PROMPT+=" $ZGP_STATUS_STAGED$ZGP_STAGED_SYM"
-      fi
-      if [[ $ZGP_STATUS_UNSTAGED -ne 0 ]]; then
-        ZGP_PROMPT+=" $ZGP_STATUS_UNSTAGED$ZGP_UNSTAGED_SYM"
-      fi
-      if [[ $ZGP_STATUS_UNTRACKED -ne 0 ]]; then
-        ZGP_PROMPT+=" $ZGP_STATUS_UNTRACKED$ZGP_UNTRACKED_SYM"
-      fi
-    fi
-  else
+  if [[ "$ZGP_IS_GIT" == false ]]; then
     ZGP_PROMPT=''
+    return
+  fi
+
+  # Branch segment
+  ZGP_PROMPT="on $ZGP_BRANCH"
+
+  if [[ $ZGP_BRANCH_HAS_UPSTREAM == false ]]; then
+    ZGP_PROMPT+=" $ZGP_BRANCH_NO_UPSTREAM_SYM"
+  elif [[ $ZGP_STATUS_BRANCH_UP_TO_DATE == true ]]; then
+    ZGP_PROMPT+=" $ZGP_BRANCH_UP_TO_DATE_SYM"
+  else
+    if [[ $ZGP_STATUS_BRANCH_AHEAD -gt 0 ]]; then
+      ZGP_PROMPT+=" $ZGP_BRANCH_AHEAD_SYM$ZGP_STATUS_BRANCH_AHEAD"
+    fi
+    if [[ $ZGP_STATUS_BRANCH_BEHIND -gt 0 ]]; then
+      ZGP_PROMPT+=" $ZGP_BRANCH_BEHIND_SYM$ZGP_STATUS_BRANCH_BEHIND"
+    fi
+  fi
+
+  # Staged and unstaged segment
+  if [[ $ZGP_STATUS_STAGED -ne 0 || $ZGP_STATUS_UNSTAGED -ne 0 || $ZGP_STATUS_UNTRACKED -ne 0 ]]; then
+    ZGP_PROMPT+=" |"
+    if [[ $ZGP_STATUS_STAGED -ne 0 ]]; then
+      ZGP_PROMPT+=" $ZGP_STATUS_STAGED$ZGP_STAGED_SYM"
+    fi
+    if [[ $ZGP_STATUS_UNSTAGED -ne 0 ]]; then
+      ZGP_PROMPT+=" $ZGP_STATUS_UNSTAGED$ZGP_UNSTAGED_SYM"
+    fi
+    if [[ $ZGP_STATUS_UNTRACKED -ne 0 ]]; then
+      ZGP_PROMPT+=" $ZGP_STATUS_UNTRACKED$ZGP_UNTRACKED_SYM"
+    fi
   fi
 }
 
-add-zsh-hook precmd gen_git_message
+add-zsh-hook precmd gen_git_prompt
 
 setopt prompt_subst
-
-# for debugging purposes
-# echo "is git: $ZGP_IS_GIT"
-# echo "branch: $ZGP_BRANCH"
-# echo "untracked: $ZGP_STATUS_UNTRACKED"
-# echo "staged: $ZGP_STATUS_STAGED"
-# echo "unstaged: $ZGP_STATUS_UNSTAGED"
-# echo "branch ahead: $ZGP_STATUS_BRANCH_AHEAD"
-# echo "branch behind: $ZGP_STATUS_BRANCH_AHEAD"
-# echo "branch up to date: $ZGP_STATUS_BRANCH_UP_TO_DATE"
 
 # TODO unset vars in zsh closing function thing
 # unset ZGP_IS_GIT
@@ -135,9 +137,6 @@ setopt prompt_subst
 # unset ZGP_STATUS_UNSTAGED
 
 # TODO
-# manage the vars export and create a default prompt string 
+# manage the vars export 
   # get inspo from omz
-
-# TODO add 'no-upstream' sym
-  # git rev-parse --abbrev-ref <branch>{upstream}
 
