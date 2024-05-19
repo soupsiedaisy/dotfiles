cf4eaf6b916d01fe4d7d51ff5fbd06fa78cdf576
Soup <dev@wildflowersoup.dev>
2024-05-19 22:21:21 +0200

    feat: add markdown linter/formatter and format readme

--- .config/nvim/after/plugin/conform.lua
+++ .config/nvim/after/plugin/conform.lua
@@ -2,6 +2,7 @@ require('conform').setup({
     formatters_by_ft = {
         lua = { 'stylua' },
         javascript = { { 'prettierd', 'prettier' } },
+        markdown = { 'markdownlint' },
     },
 
     -- format_on_save = { timeout_ms = 500, lsp_fallback = true },
