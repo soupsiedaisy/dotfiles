17a246a3b6aa6bbbd2a1881f8d48ec29f7692705
Willa Charlotte Lyle <willa@bbd.co.za>
2024-05-23 10:42:02 +0200

    feat: add /opt/nvim... to path

--- .config/zsh/zsh_exports.zsh
+++ .config/zsh/zsh_exports.zsh
@@ -1,7 +1,7 @@
-export VISUAL=$(which vim)
-export EDITOR=$VISUAL
 path+=("$HOME/.local/bin")
 path+=("$HOME/.local/share/jack")
+path=("/opt/nvim-linux64/bin" $path)
 export PATH
-
+export VISUAL=$(which vim)
+export EDITOR=$VISUAL
 
