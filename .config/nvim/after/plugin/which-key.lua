cf20dffd51cb22753b4a2c2eaa2fa49641879ed3
Willa <dev@wildflowersoup.dev>
2025-02-12 12:07:04 +0200

    feat: update which key reg method and add _? keybind

---
+++ .config/nvim/after/plugin/which-key.lua
@@ -0,0 +1,12 @@
+-- TODO add custom mappings
+require('which-key').add(
+    {
+        {
+            "<leader>?",
+            function()
+                require("which-key").show({ global = false })
+            end,
+            desc = "Buffer Local Keymaps (which-key)",
+        },
+    }
+)
