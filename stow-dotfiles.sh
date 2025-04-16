3382adfee672e4ddd3de60e05e43e932d1a93973
Willa Charlotte Lyle <willa@bbd.co.za>
2025-04-16 11:47:18 +0200

    feat: update stow-dotfiles.sh to conditionally set --no-folding and to pass parameters through to stow call as well as add local stow ignore files

--- stow-dotfiles.sh
+++ stow-dotfiles.sh
@@ -1,11 +1,23 @@
 #!/usr/bin/env bash
 
 STOW_DIRS=$(cat .stow-directories)
+ARGS=$*
 
-for dir in $(echo $STOW_DIRS | sed "s/,/ /g")
-do
-    echo "stowing $dir"
-    stow -R $dir
+check-no-folding () {
+    if [[ -f $1/.stow-no-folding ]]; then
+        NO_FOLD="--no-folding"
+    else
+        unset NO_FOLD
+    fi
+}
+
+if [[ -z $ARGS ]]; then
+    ARGS='-R'
+fi
+
+for dir in $(echo $STOW_DIRS | sed "s/,/ /g"); do
+    check-no-folding $dir
+    echo "running 'stow $ARGS $NO_FOLD $dir'"
+    stow $* $dir
 done
-
 
