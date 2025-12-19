9ce753d4014bf9da3063511283f89446a0dcd7a7
Willa Charlotte Lyle <dev@wildflowersoup.dev>
2025-12-19 11:15:53 +0200

    feat: add typescript_tools plugin; add automated install for linters/formatters; clean up after/plugin/mason.lua

--- nvim/.config/nvim/after/plugin/mason.lua
+++ nvim/.config/nvim/after/plugin/mason.lua
@@ -1,3 +1,10 @@
+local mason = require('mason')
+local mason_registry = require('mason-registry')
+local telescope_builtin = require('telescope.builtin')
+local cmp_nvim_lsp = require('cmp_nvim_lsp')
+local mason_lspconfig = require('mason-lspconfig')
+local typescript_tools = require('typescript-tools')
+
 local on_attach = function(_, bufnr)
     local nmap = function(keys, desc, func)
         if desc then
@@ -12,25 +19,27 @@ local on_attach = function(_, bufnr)
     end
 
     nmap('gD', 'Goto declaration', vim.lsp.buf.declaration)
-    nmap('gd', 'Goto definition', require('telescope.builtin').lsp_definitions)
-    nmap('gr', 'Goto references', require('telescope.builtin').lsp_references)
-    nmap('gI', 'Goto implementation', require('telescope.builtin').lsp_implementations)
+    nmap('gd', 'Goto definition', telescope_builtin.lsp_definitions)
+    nmap('gr', 'Goto references', telescope_builtin.lsp_references)
+    nmap('gI', 'Goto implementation', telescope_builtin.lsp_implementations)
     nmap('K', 'Hover documentation', vim.lsp.buf.hover)
     nmap('<C-k>', 'Signature documentation', vim.lsp.buf.signature_help)
     nmap('<leader>lr', 'Rename', vim.lsp.buf.rename)
     nmap('<leader>la', 'Code action', vim.lsp.buf.code_action)
-    nmap('<leader>lD', 'Type definition', require('telescope.builtin').lsp_type_definitions)
-    nmap('<leader>ls', 'Document symbols', require('telescope.builtin').lsp_document_symbols)
-    nmap('<leader>lws', 'Workspace symbols', require('telescope.builtin').lsp_dynamic_workspace_symbols)
+    nmap('<leader>lD', 'Type definition', telescope_builtin.lsp_type_definitions)
+    nmap('<leader>ls', 'Document symbols', telescope_builtin.lsp_document_symbols)
+    nmap('<leader>lws', 'Workspace symbols', telescope_builtin.lsp_dynamic_workspace_symbols)
     nmap('<leader>lwa', 'Workspace add folder', vim.lsp.buf.add_workspace_folder)
     nmap('<leader>lwr', 'Workspace remove folder', vim.lsp.buf.remove_workspace_folder)
     nmap('<leader>lwl', 'Workspace list folders', listWorkspaceFolders)
 end
 
-require('mason').setup()
-require('mason-lspconfig').setup()
-
-local servers = {
+local language_servers = {
+    clangd = {},
+    eslint = {},
+    harper_ls = {},
+    html = {},
+    jsonls = {},
     lua_ls = {
         Lua = {
             workspace = {
@@ -40,37 +49,60 @@ local servers = {
                 globals = {
                     'vim',
                     'require',
+                    'package',
                 },
             },
             telemetry = { enable = false },
         },
     },
-    ts_ls = {},
-    taplo = {},
-    omnisharp = {},
-    somesass_ls = {},
-    eslint = {},
-    harper_ls = {},
-    html = {},
-    jsonls = {},
     pylsp = {},
+    somesass_ls = {},
+    stylua = {},
+    taplo = {},
+    ts_ls = {},
 }
 
-local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
+local linters = {
+    'markdownlint',
+    'prettier',
+    'eslint_d',
+    'prettierd',
+}
 
-local mason_lspconfig = require('mason-lspconfig')
+mason.setup()
+
+for _, linter in pairs(linters) do
+    local package = mason_registry.get_package(linter)
+
+    if package.is_installable(package) and not package.is_installed(package) then
+        package.install(package)
+    end
+end
 
 mason_lspconfig.setup({
-    ensure_installed = vim.tbl_keys(servers),
+    ensure_installed = vim.tbl_keys(language_servers),
     automatic_enable = true,
 })
 
-for server in pairs(servers) do
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities = cmp_nvim_lsp.default_capabilities(capabilities)
+
+for server, config in pairs(language_servers) do
     vim.lsp.config(server, {
-        settings = servers[server],
+        settings = config,
         capabilities = capabilities,
         on_attach = on_attach,
-        filetypes = (servers[server] or {}).filetypes,
+        filetypes = config.filetypes,
     })
 end
+
+-- TODO integrate with ts_ls
+typescript_tools.setup({
+    settings = {
+        tsserver_plugins = {
+            '@styled/typescript-styled-plugin',
+        },
+    },
+    capabilities = capabilities,
+    on_attach = on_attach,
+})
