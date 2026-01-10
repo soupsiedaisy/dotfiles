50eb778290502c0d073ed310b4602e0c6291d743
Soup <dev@wildflowersoup.dev>
2026-01-10 18:53:10 +0200

    feat: set nvim theme to carbonfox

--- nvim/.config/nvim/lua/plugins/colorschemes.lua
+++ nvim/.config/nvim/lua/plugins/colorschemes.lua
@@ -1,15 +1,15 @@
 return {
     { 'catppuccin/nvim', name = 'catppuccin', priority = 1000 },
     { 'neanias/everforest-nvim', priority = 1000 },
-    { 'EdenEast/nightfox.nvim', priority = 1000 },
+    { 'navarasu/onedark.nvim', priority = 1000 },
     { 'Yazeed1s/oh-lucy.nvim', priority = 1000 },
     { 'rose-pine/neovim', priority = 1000 },
     { 'folke/tokyonight.nvim', priority = 1000 },
     {
-        'navarasu/onedark.nvim',
+        'EdenEast/nightfox.nvim',
         priority = 1000,
         config = function()
-            vim.cmd.colorscheme('onedark')
+            vim.cmd.colorscheme('carbonfox')
         end,
     },
 }
