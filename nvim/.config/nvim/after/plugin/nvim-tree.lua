16284f4206d70e0e4001d3aadf796f6c633cb562
Soup <dev@wildflowersoup.dev>
2026-01-10 19:00:01 +0200

    feat: move nvim-tree to right side and add width properties

--- nvim/.config/nvim/after/plugin/nvim-tree.lua
+++ nvim/.config/nvim/after/plugin/nvim-tree.lua
@@ -2,6 +2,14 @@ require('nvim-tree').setup({
     view = {
         number = true,
         relativenumber = true,
+        side = 'right',
+        width = {
+            min = 20,
+            max = function()
+                return math.floor(vim.o.columns / 3)
+            end,
+            padding = 1,
+        },
     },
     update_focused_file = {
         enable = true,
