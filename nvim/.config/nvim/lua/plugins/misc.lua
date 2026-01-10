d33f80be0897d1d352004f8972e77b1ae17c0f0c
Soup <dev@wildflowersoup.dev>
2026-01-10 18:54:24 +0200

    feat: add vim-surround plugin and remove whitespace and comments from misc.lua

--- nvim/.config/nvim/lua/plugins/misc.lua
+++ nvim/.config/nvim/lua/plugins/misc.lua
@@ -1,8 +1,6 @@
 return {
-    -- Git related plugins
     'tpope/vim-fugitive',
     'tpope/vim-rhubarb',
-
-    -- Detect tabstop and shiftwidth automatically
     'tpope/vim-sleuth',
+    'tpope/vim-surround',
 }
